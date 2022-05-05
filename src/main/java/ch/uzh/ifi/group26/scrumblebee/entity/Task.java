fb895af35e5e64dc6c4f6cbfa6629bb3a68121c9
nicomantzanas <nico.mantzanas@uzh.ch>
2022-05-05 18:20:31 +0200

    Implemented TaskServiceIntegrationTest - closes #231

--- src/main/java/ch/uzh/ifi/group26/scrumblebee/entity/Task.java
+++ src/main/java/ch/uzh/ifi/group26/scrumblebee/entity/Task.java
@@ -33,7 +33,7 @@ public class Task implements Serializable {
     @Column(nullable = false)
     private String title;
 
-    @Column(nullable = false)
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String description;
 
     @Column(nullable = false)
